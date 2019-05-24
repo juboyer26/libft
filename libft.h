51851b428194a9f421dbf92ff74b939c66d52ad4
Justine BOYER <juboyer@c4r8s7.wethinkcode.co.za>
2019-05-24 08:25:18 +0200

    updated

---
+++ libft.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juboyer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:14:21 by juboyer           #+#    #+#             */
+/*   Updated: 2019/05/24 07:30:44 by juboyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_strlen(char *str);
+int		ft_atoi(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+char    *ft_strcat(char *s1, const char *s2);
+char    *ft_strncat(char *s1, const char *s2, size_t n);
+char    *ft_strrchr(const char *s, int c);
+int     ft_isalpha(int c);
+int     ft_isalnumb(int c);
+char    *ft_strdup(const char *s1);
+int     ft_isascii(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
+
+#endif
