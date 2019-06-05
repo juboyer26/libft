a155e3832ffa293a1ca6a3b8c180900b397cc26b
Justine BOYER <juboyer@c4r7s6.wethinkcode.co.za>
2019-06-05 16:34:45 +0200

    complete

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: juboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 13:54:57 by juboyer           #+#    #+#             */
-/*   Updated: 2019/06/04 13:53:12 by juboyer          ###   ########.fr       */
+/*   Updated: 2019/06/05 16:17:53 by juboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	len;
-	size_t	len2;
-	int		result;
-
-	len = ft_strlen((char *)s1);
-	len2 = ft_strlen((char *)s2);
-	len = (len <= len2) ? len + 1 : len2 + 1;
-	len = (len <= n) ? len : n;
-	result = ft_memcmp(s1, s2, len);
-	return (result);
+	if ( n == 0)
+		return (0);
+	while ((unsigned char)*s1 == (unsigned char)*s2 && *s1 != 0 & n-- > 1)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
--- mainall.c
+++ mainall.c
@@ -6,7 +6,7 @@
 /*   By: juboyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:35:22 by juboyer           #+#    #+#             */
-/*   Updated: 2019/06/03 12:51:39 by juboyer          ###   ########.fr       */
+/*   Updated: 2019/06/05 13:58:10 by juboyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -302,4 +302,43 @@ int		main(void)
 {
 	puts(ft_itoa(101));
 	return (0);
+}
+
+ft_bzero.c
+
+int main () {
+
+
+    char str[] = "abcdcdee";
+    int c = 6;
+
+
+    ft_bzero(str, c);
+    printf("String after first |%d| bytes changes to 0 is - |%s|\n", c, str);
+
+
+   return(0);
+}
+
+ft_memcmp.c
+
+int main () {
+   char str1[15];
+   char str2[15];
+   int ret;
+
+   ft_memcpy(str1, "abcdef", 6);
+   ft_memcpy(str2, "ABCDEF", 6);
+
+   ret = ft_memcmp(str1, str2, 5);
+
+   if(ret > 0) {
+      printf("str2 is less than str1");
+   } else if(ret < 0) {
+      printf("str1 is less than str2");
+   } else {
+      printf("str1 is equal to str2");
+   }
+
+   return(0);
 }
